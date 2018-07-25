0bb0b7639c83b22b8210be8da7cfb21740633c89
Alexander Mahabir <alex.mahabir@alfresco.com>
2018-07-25 10:06:13 -0400

    updated to support 1 solr6 and acs with embedded solr6

--- scripts/run-share-na.sh
+++ scripts/run-share-na.sh
@@ -5,10 +5,10 @@ systemctl stop mysql-default
 systemctl disable mysql-default
 yum remove -y mysql-server
 rm -fr /etc/mysql-default* /var/lib/mysql* /var/log/mysql-default* /usr/lib/systemd/system/mysql-default.service
-service solr stop
-chkconfig --del solr
-rm -rf /opt/alfresco-search-services /var/solr /etc/init.d/solr /etc/sysconfig/solr /var/log/solr /etc/default/solr.in.sh
-userdel -r solr
+#service solr stop
+#chkconfig --del solr
+#rm -rf /opt/alfresco-search-services /var/solr /etc/init.d/solr /etc/sysconfig/solr /var/log/solr /etc/default/solr.in.sh
+#userdel -r solr
 rm -fr /etc/chef/chef-client.json
 rm -fr /etc/chef/nodes/*.json
 chmod 700 /usr/share/tomcat/shared/classes/alfresco-global.properties
